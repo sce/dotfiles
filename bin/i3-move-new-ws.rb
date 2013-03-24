dac73c518ba35f99f77ae96e88105a0baa98590c
S. Christoffer Eliesen <christoffer@eliesen.no>
2013-03-24 11:50:44 +0100

    bin/i3-{,move-}new-ws.rb: Merge to a single script.

--- bin/i3-move-new-ws.rb
+++ bin/i3-move-new-ws.rb
@@ -1,5 +1,1 @@
-#!/usr/bin/env ruby
-require 'json'
-
-last = JSON.parse(%x(i3-msg -t get_workspaces)).map { |ws| ws["num"].to_i }.sort.last
-%x(i3-msg move container to workspace #{last + 1})
+i3-new-ws.rb
--- bin/i3-new-ws.rb
+++ bin/i3-new-ws.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
 require 'json'
 
-last = JSON.parse(%x(i3-msg -t get_workspaces)).map { |ws| ws["num"].to_i }.sort.last
-%x(i3-msg workspace #{last + 1})
+wss = JSON.parse(%x(i3-msg -t get_workspaces))
+last_ws = wss.map { |ws| ws["num"].to_i }.sort.last
+new_ws = last_ws + 1
+
+if $0 =~ /i3-move-new-ws\.rb\z/
+  %x(i3-msg move container to workspace #{new_ws})
+else
+  %x(i3-msg workspace #{new_ws})
+end
