8073148d2bf3e1b8e66d58d776b5f2c984efbe48
S. Christoffer Eliesen <cel@io.no>
2014-01-23 12:23:08 +0100

    bin/activesink.sh: Refactor.

--- bin/activesink.sh
+++ bin/activesink.sh
@@ -2,19 +2,46 @@
 
 ACTIVE_SINK=`pacmd list-sinks|awk '/* index: /{print $3}'`
 
-VOLUP="pactl set-sink-volume $ACTIVE_SINK -- +3%"
-VOLDOWN="pactl set-sink-volume $ACTIVE_SINK -- -3%"
-MUTETOGGLE="pactl set-sink-mute $ACTIVE_SINK toggle"
+function set_curlevel {
+  CURLEVEL=$(pacmd list-sinks|grep -A 15 '* index'|awk '/volume:/{ print $5 }'|head -1)
+}
+
+function set_muted {
+  if [ ! "$MUTED" ]; then
+    MUTED=$(pacmd list-sinks|grep -A 15 '* index'|awk '/muted: /{ print $2 }')
+    [ "$MUTED" == "yes" ] && MUTED_TEXT="(MUTED)" || unset MUTED_TEXT
+  fi
+}
+
+function volume_text {
+  set_curlevel
+  set_muted
+  echo "Volume $CURLEVEL $MUTED_TEXT"
+}
+
+function notify_volume {
+  notify-send -u low "$(volume_text)"
+}
+
+INC="3%"
+function volup { pactl set-sink-volume $ACTIVE_SINK -- +$INC; }
+function voldown { pactl set-sink-volume $ACTIVE_SINK -- -$INC; }
+function mutetoggle { pactl set-sink-mute $ACTIVE_SINK toggle; unset MUTED; }
 
 case "$0" in
   *up)
-    ($VOLUP)
+    volup
+    notify_volume
     ;;
+
   *down)
-    ($VOLDOWN)
+    voldown
+    notify_volume
     ;;
+
   *mutetoggle)
-    ($MUTETOGGLE)
+    mutetoggle
+    notify_volume
     ;;
   *)
     echo $ACTIVE_SINK
