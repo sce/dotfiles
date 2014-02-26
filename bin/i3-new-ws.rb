8d05f7ed56ca3e6f59bb9042f212024b8c1aadfb
S. Christoffer Eliesen <cel@io.no>
2014-02-26 10:10:23 +0100

    bin/i3-new-ws.rb: Better var names.

--- bin/i3-new-ws.rb
+++ bin/i3-new-ws.rb
@@ -4,9 +4,9 @@ require 'json'
 wss = JSON.parse(%x(i3-msg -t get_workspaces))
 ws_nums = wss.map { |ws| ws["num"].to_i }.sort
 
-open_ws = nil
-is_open = ws_nums.find.each_with_index { |num, i| num != (open_ws = i+1) }
-new_ws = is_open && open_ws || ws_nums.last + 1
+gap_ws = nil
+gap_exists = ws_nums.find.each_with_index { |num, i| num != (gap_ws = i+1) }
+new_ws = gap_exists && gap_ws || ws_nums.last + 1
 
 if $0 =~ /i3-move-new-ws\.rb\z/
   %x(i3-msg move container to workspace #{new_ws})
