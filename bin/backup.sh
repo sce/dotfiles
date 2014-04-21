b81204f5cad7a8aae110722524e8094bc1e598fa
S. Christoffer Eliesen <cel@io.no>
2014-04-21 11:36:40 +0200

    bin/backup.sh: Add. backup.exclude: Add.

---
+++ bin/backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# This script will do an rsync backup of SRC directory to HOST.
+# It will place the backup in a timestamp directory in DEST_ROOT and create a
+# latest link to that directory when finished.
+#
+# When run multiple times the latest symlink will be used as --link-dest
+# causing new backups to have hardlinked copies of equal files to that of the
+# backup before it.
+#
+# Trailing arguments to this script is passed on to rsync. That's useful for
+# passing on e.g. --dry-run.
+#
+# List of patterns in backup.exclude are excluded from backup.
+
+# We use a full datetime timestamp to make sure we can do multiple backups per
+# day (for whatever reason) without collisions.
+TS=$(date +%Y%m%d.%H%M%S)
+
+HOST=
+SRC=
+EXCLUDE_FROM=backup.exclude
+LOG_FILE=backup.log
+
+DEST_ROOT=
+DEST_DIR=$DEST_ROOT/$TS
+LINK_DEST=$DEST_ROOT/latest
+
+DEST=$HOST:$DEST_DIR
+
+[ -z "$HOST" -o -z "$SRC" -o -z "$DEST_ROOT" ] && exit 1
+
+# -a archive
+# -h human readable units
+# -v verbose
+# -i show how files are changed
+# -z compress during transfer
+# -H find/transfer hard links
+
+# --progress show progress during transfer
+# --link-dest remote directory to hardlink to when file is unmodified
+# --exclude-from file with exclude filters
+
+OPTS="-ahvizH --progress --log-file=$LOG_FILE --link-dest=$LINK_DEST --exclude-from=$EXCLUDE_FROM $@"
+
+rsync $OPTS $SRC $DEST
+
+# Update link to latest (if trailing --dry-run was given then DEST_DIR won't
+# exist, and the link won't be updated).
+ssh $HOST "cd $DEST_ROOT && [ -d \"$TS\" ] && ln -snf $TS $LINK_DEST || echo Could not find $DEST_DIR: Not updating latest."
