f4af4849ad298d26b5f2aca0c4121b95b695def5
S. Christoffer Eliesen <christoffer@eliesen.no>
2014-04-26 17:26:26 +0200

    bin/backup.sh: Handle rsync errors better.

--- bin/backup.sh
+++ bin/backup.sh
@@ -2,7 +2,8 @@
 
 # This script will do an rsync backup of SRC directory to HOST.
 # It will place the backup in a timestamp directory in DEST_ROOT and create a
-# latest link to that directory when finished.
+# latest link to that directory when finished. The destination directory has an
+# "incomplete" suffix which is removed after rsync-ing is done.
 #
 # When run multiple times the latest symlink will be used as --link-dest
 # causing new backups to have hardlinked copies of equal files to that of the
@@ -14,7 +15,10 @@
 # List of patterns in backup.exclude are excluded from backup.
 
 HOST=
-SRC=
+SRC= # /home/$(whoami)/
+# This will put the incremental backups inside $(whoami):
+DEST_ROOT= # /var/backups/$(hostname)/$(whoami)
+
 EXCLUDE_FROM=backup.exclude
 LOG_FILE=backup.log
 
@@ -22,9 +26,14 @@ LOG_FILE=backup.log
 # day (for whatever reason) without collisions.
 TS=$(date +%Y%m%d.%H%M%S)
 
-DEST_ROOT=
-DEST_DIR=$DEST_ROOT/$TS
+# Where rsync will look for similar files it can hardlink to instead of sending
+# over the wire:
 LINK_DEST=$DEST_ROOT/latest
+
+# Destination directory has an "incomplete" suffix which we remove after we're
+# done.
+INCOMPLETE=$TS.incomplete
+DEST_DIR=$DEST_ROOT/$INCOMPLETE
 
 DEST=$HOST:$DEST_DIR
 
@@ -45,8 +54,14 @@ ssh $HOST "[ -d \"$DEST_ROOT\" ] || mkdir -p $DEST_ROOT"
 
 OPTS="-ahvizH --progress --log-file=$LOG_FILE --link-dest=$LINK_DEST --exclude-from=$EXCLUDE_FROM $@"
 
-rsync $OPTS $SRC $DEST
+rsync $OPTS $SRC $DEST &&
 
 # Update link to latest (if trailing --dry-run was given then DEST_DIR won't
 # exist, and the link won't be updated).
-ssh $HOST "cd $DEST_ROOT && [ -d \"$TS\" ] && ln -snf $TS $LINK_DEST || echo Could not find $DEST_DIR: Not updating latest."
+ssh $HOST "(
+  cd $DEST_ROOT && [ -d \"$INCOMPLETE\" ] &&
+  mv -iv $INCOMPLETE $TS && ln -snfv $TS $LINK_DEST ||
+  echo \"Could not find $DEST_DIR: Not updating latest.\"
+)" ||
+
+echo "$0: rsync failed with $?: Not updating $LINK_DEST."
