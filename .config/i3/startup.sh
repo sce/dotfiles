5b4b380cecd62905527bc6537637db3d6b9d134e
S. Christoffer Eliesen <christoffer@eliesen.no>
2015-03-23 22:50:11 +0100

    .config/i3/startup.sh: Add, use i3/startup dir with symlinks.

---
+++ .config/i3/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+# ( http://stackoverflow.com/a/246128 )
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+for prog in $(find $dir/startup -type f -o -type l); do
+  echo $prog && $prog &
+done
