9be61a5e4b1f781b38992fc3bd798062cc18583d
S. Christoffer Eliesen <christoffer@eliesen.no>
2016-08-27 18:09:39 +0200

    bin7xrandr-rad-quad-uw: Adjust.

--- bin/xrandr-rad-quad-uw.sh
+++ bin/xrandr-rad-quad-uw.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-LEFT=DisplayPort-0
+TOP=DisplayPort-0
 BOTTOM=DisplayPort-1
+
+LEFT=DVI-1
 
 # HDMI-0 and DVI-0 share the same graphical output (only one can be used at a time).
 RIGHT=HDMI-0
 #BOTTOM=DVI-0
-
-TOP=DVI-1
 
 # The edge monitors have slightly less vertical resolution than the middle monitors combined,
 # so we move the edge monitors down a little bit so that they are centered on the middle ones.
@@ -23,7 +23,7 @@ sleep 0.2
 
 xrandr \
   --output $LEFT --rotate right --pos 0x${EDGE_Y} --auto \
-  --output $BOTTOM --pos 1080x1080 --auto \
+  --output $BOTTOM --pos 1080x1080 --auto --primary \
   --output $TOP --above $BOTTOM --auto \
   --output $RIGHT --pos ${RIGHT_X}x${EDGE_Y} --rotate left --auto &&
 
