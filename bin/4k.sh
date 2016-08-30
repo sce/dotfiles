d14b8c6ad91e114884e0cb002990e0db27c70b6d
S. Christoffer Eliesen <christoffer@eliesen.no>
2016-08-30 12:00:27 +0200

    bin/4k: Three monitors.

--- bin/4k.sh
+++ bin/4k.sh
@@ -2,32 +2,44 @@
 
 main=eDP-1
 second=HDMI-1
+third=DP-2
 
 #dpi=220
-dpi=144
 dpi=96
+dpi=144
 
 # built-in laptop monitor:
-res=1920x1440
-res=2048x1536
-res=1856x1392
-res=1792x1344
+main_res=1920x1440
+main_res=2048x1536
+main_res=1856x1392
+main_res=1792x1344
 
-res=1600x1200
-res=3840x2160
-xrandr --output $main --auto --mode $res --dpi $dpi
+main_res=1600x1200
+main_res=3840x2160
+
+# second monitor: physically bigger (27"), so we want this to be primary:
+sec_res=1920x1200
+sec_res=1920x1080
+sec_res=3840x2160
+
+# third monitor: 24":
+th_res=1920x1200
+th_res=1920x1080
+xrandr --output $main --auto --mode $main_res --dpi $dpi
 
 sleep 1
 
-# second monitor: physically bigger, so we want this to be primary:
-res=1920x1200
-res=1920x1080
-res=3840x2160
 # using auto here will "turn off" the display if the monitor is disconnected:
 xrandr --output $second --auto --primary
+xrandr --output $third --auto
+
+sleep 1
 
 # but if it's conncted we want to configure it properly:
-xrandr --output $second --left-of $main --mode $res --auto --dpi $dpi --primary
+# (Machine doesn't support more than 8k horizontal resolution... so turn off main screen)
+xrandr --output $second --left-of $main --mode $sec_res --auto --dpi $dpi --primary &&
+  xrandr --output $main --off &&
+  xrandr --output $third --left-of $second --mode $th_res --auto --dpi 96
 
 echo Done.
 
