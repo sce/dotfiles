8ee4d6880b9ff0b9c83b6c9296fb3c03c8317f74
S. Christoffer Eliesen <christoffer@eliesen.no>
2016-09-05 10:15:25 +0200

    bin/4k.sh: Scale down main monitor so that it fits third.

--- bin/4k.sh
+++ bin/4k.sh
@@ -25,28 +25,28 @@ sec_res=3840x2160
 # third monitor: 24":
 th_res=1920x1080
 th_res=1920x1200 # native
-xrandr --output $main --auto --mode $main_res --dpi $big_dpi
 
-sleep 1
+script=$(basename $0)
 
-# using auto here will "turn off" the display if the monitor is disconnected:
-#xrandr --output $second --auto --primary
-#xrandr --output $third --auto
+  # 1440 / 1200 = 0.83333
+  xrandr --output $main --auto --mode $main_res --dpi $big_dpi --scale 1x0.833
 
-# the display driver can get confused when add/removing displays, so better turn off and on again:
-xrandr --output $second --off
-xrandr --output $third --off
+  sleep 1
 
-sleep 1
+  # the display driver can get confused when add/removing displays, so better turn off and on again:
+  xrandr --output $second --off
+  xrandr --output $third --off
 
-# but if it's conncted we want to configure it properly:
-# (Machine doesn't support more than 8k horizontal resolution... so turn off main screen)
-# We need to scale the third monitor so the system thinks it has twice as big
-# resolution. Doing this will prevent the interface from being a nice size on
-# the 4k monitor and way too big on the third one.
-# hmm, maybe just mirror the laptop monitor onto third display and scale appropriately?
-xrandr --output $second --right-of $main --mode $sec_res --auto --dpi $big_dpi --primary &&
-  xrandr --output $third --same-as $main --dpi 96 --mode $th_res --auto --scale 1x1.2
+  sleep 1
+
+  # but if it's conncted we want to configure it properly:
+  # (Machine doesn't support more than 8k horizontal resolution... so turn off main screen)
+  # We need to scale the third monitor so the system thinks it has twice as big
+  # resolution. Doing this will prevent the interface from being a nice size on
+  # the 4k monitor and way too big on the third one.
+  # hmm, maybe just mirror the laptop monitor onto third display and scale appropriately?
+  xrandr --output $second --right-of $main --mode $sec_res --auto --dpi $big_dpi --primary &&
+    xrandr --output $third --same-as $main --dpi 96 --mode $th_res --auto
 
 echo Done.
 
