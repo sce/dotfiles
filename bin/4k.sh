877ad192f18e4e78b2e2b1ef32cc0453abc7ff56
S. Christoffer Eliesen <christoffer@eliesen.no>
2017-09-28 17:37:57 +0200

    bin/4k: More descriptive variables.

--- bin/4k.sh
+++ bin/4k.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 main=${MAIN:-eDP-1}
-second=${VERTICAL:-HDMI-1}
-third=${UWIDE:-DP-2}
+dell=${VERTICAL:-HDMI-1}
+hp=${UWIDE:-DP-2}
 
 #big_dpi=220
 big_dpi=96
@@ -17,14 +17,14 @@ main_res=1600x1200
 main_res=1920x1440
 main_res=3840x2160
 
-# second monitor: physically bigger (27"), so we want this to be primary:
-sec_res=1920x1200
-sec_res=1920x1080
-sec_res=3840x2160
+# dell monitor: physically bigger (27"), so we want this to be primary:
+dell_res=1920x1200
+dell_res=1920x1080
+dell_res=3840x2160
 
-# third monitor: 24":
-th_res=1920x1080
-th_res=1920x1200 # native
+# hp monitor: 24":
+hp_res=1920x1080
+hp_res=1920x1200 # native
 
 script=$(basename $0)
 
@@ -34,7 +34,7 @@ if [ "$script" == "4k.sh" ]; then
   if [ "$1" == "fullhd" ]; then
     scale="--scale-from 1920x1080"
     rightof="--pos 1920x0"
-    sec_res="1920x1080"
+    dell_res="1920x1080"
   else
     scale="--transform none"
     scale="--scale-from 2560x1440"
@@ -50,26 +50,26 @@ if [ "$script" == "4k.sh" ]; then
   sleep 1
 
   # using auto here will "turn off" the display if the monitor is disconnected:
-  #xrandr --output $second --auto --primary
-  #xrandr --output $third --auto
+  #xrandr --output $dell --auto --primary
+  #xrandr --output $hp --auto
 
   # the display driver can get confused when add/removing displays, so better turn off and on again:
-  xrandr --output $second --off --transform none
-  xrandr --output $third --off --transform none
+  xrandr --output $dell --off --transform none
+  xrandr --output $hp --off --transform none
 
   sleep 1
 
   # but if it's conncted we want to configure it properly:
   # (Machine doesn't support more than 8k horizontal resolution... so turn off main screen)
-  # We need to scale the third monitor so the system thinks it has twice as big
+  # We need to scale the hp monitor so the system thinks it has twice as big
   # resolution. Doing this will prevent the interface from being a nice size on
-  # the 4k monitor and way too big on the third one.
-  # hmm, maybe just mirror the laptop monitor onto third display and scale appropriately?
-  # xrandr --output $third --same-as $main --dpi 96 --mode $th_res --auto
-  xrandr --output $third $rightof --mode $th_res --auto --rotate left
+  # the 4k monitor and way too big on the hp one.
+  # hmm, maybe just mirror the laptop monitor onto hp display and scale appropriately?
+  # xrandr --output $hp --same-as $main --dpi 96 --mode $hp_res --auto
+  xrandr --output $hp $rightof --mode $hp_res --auto --rotate left
   sleep 0.2
-  #xrandr --output $second --same-as $main --dpi 96 --mode $sec_res --auto --dpi $big_dpi --primary
-  xrandr --output $second --same-as $main --dpi 96 --mode $sec_res --auto --dpi $big_dpi --primary $scale
+  #xrandr --output $dell --same-as $main --dpi 96 --mode $dell_res --auto --dpi $big_dpi --primary
+  xrandr --output $dell --same-as $main --dpi 96 --mode $dell_res --auto --dpi $big_dpi --primary $scale
 
 elif [ "$script" == "4k-single.sh" ]; then
   if [ "$1" == "fullhd" ]; then
@@ -79,8 +79,8 @@ elif [ "$script" == "4k-single.sh" ]; then
   fi
 
   xrandr \
-    --output $second --off \
-    --output $third --off \
+    --output $dell --off \
+    --output $hp --off \
     --output $main --auto --mode $main_res $scale
 
 else
