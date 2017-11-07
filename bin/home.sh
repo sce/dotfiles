037a8cf91e4bc60dedd4f2776ced66e3d03785e9
S. Christoffer Eliesen <christoffer@eliesen.no>
2017-11-07 20:13:50 +0100

    bin/home.sh: Show everything on main screen.

--- bin/home.sh
+++ bin/home.sh
@@ -19,37 +19,25 @@ xrandr \
   --output $UWIDE --off --transform none \
   --output $VERTICAL --off --transform none \
   --output $MAIN --auto --transform none
-  # --output $MAIN --auto --scale-from 2560x1440
-  #--output $MAIN --mode 3840x2160 --scale-from 2560x1440
-  # 1440 will create a big space underneith the display that's empty/wallpaper,
-  # but it keeps the remaining picture scaled to correct ratio (16:9)
-
 sleep 3
 
 echo "2) initial setup complete, setting up first extra screen ..."
 
-#xrandr --output $VERTICAL --auto --left-of $MAIN --rotate right
-  # --output $MAIN --auto --scale-from 2560x1080 --pos 0x0 \
 xrandr \
   --output $UWIDE --auto --pos 0x0 \
   --output $MAIN --off
-  #--output $MAIN --pos 0x0 \
-  #--output $UWIDE --auto --mode 2560x1080 --same-as $MAIN
-  # --output $VERTICAL --auto --pos 2560x0 --rotate right
 
 sleep 2
 
 echo "3) setting up second extra screen ..."
 
-#xrandr --output $MAIN --scale-from 2560x1080 --output $UWIDE --auto
 # using --left-of for hdmi-1 refuses to work ...
+# 2560x1440 will create a big space underneath the display that's empty/wallpaper,
+# but it keeps the remaining picture scaled to correct ratio (16:9)
+# --output $MAIN --auto --scale-from 2560x1440
 xrandr \
   --output $UWIDE --auto \
   --output $VERTICAL --auto --pos 2560x0 --rotate left \
-  --output $MAIN --auto --scale-from 2560x1440
-  #--output $VERTICAL --auto --right-of $UWIDE --rotate left
-
-#xrandr \
-  #--output $UWIDE --auto --pos 1080x0
+  --output $MAIN --auto
 
 echo "4) Done"
