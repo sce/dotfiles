e8964b593e6da9a3833ece9926dcbf5a4e405482
S. Christoffer Eliesen <christoffer@eliesen.no>
2021-08-01 11:31:00 +0700

    bin/sway-new-ws: Use alphanumeric workspace names.

--- bin/sway-new-ws.rb
+++ bin/sway-new-ws.rb
@@ -7,12 +7,25 @@ def notify msg
 	%x(notify-send -t 500 "#{msg}")
 end
 
+# returns hexadecimal string version of number
 def hex(num)
   num.to_s(16)
 end
 
+# returns number parsed from hexadecimal string
 def from_hex(str)
   str.to_s.hex
+end
+
+def ws_name(num)
+  return num
+
+  hexversion = hex(num)
+  if num != hexversion
+    "#{num}: #{hexversion}"
+  else
+    num
+  end
 end
 
 WS=%r(sway-ws\.rb\z)
@@ -23,7 +36,7 @@ pp wss = JSON.parse(%x(swaymsg -t get_workspaces))
 
 # Workspaces that are not "numeric" (ie. whos name doesn't start with a number)
 # are given the number -1 in i3.
-# pp ws_nums = wss.map { |ws| from_hex(ws["num"]) }.reject {|num| num < 0 }.sort
+# pp ws_nums = wss.map { |ws| ws["num"] }.reject {|num| num < 0 }.sort
 pp ws_names = wss.map { |ws| from_hex(ws["name"]) }.sort
 
 if $0 =~ WS
@@ -35,8 +48,11 @@ if $0 =~ WS
 	end
 else
 	gap_ws = nil
-    gap_exists = ws_names.find.each_with_index { |name, i| name.to_s != (gap_ws = hex(i+1)) }
-    new_ws = gap_exists && gap_ws || hex(from_hex(ws_names.last) + 1)
+    #gap_exists = ws_nums.find.each_with_index { |num, i| num != (gap_ws = i+1) }
+    gap_exists = ws_names.find.each_with_index { |name, i| hex(name) != (gap_ws = hex(i+1)) }
+    new_ws = gap_exists && gap_ws || hex(ws_names.last + 1)
+    #new_ws = gap_exists && gap_ws || (ws_nums.last + 1)
+    #new_ws = ws_name(new_ws)
 
 	if $0 =~ MOVE_TO_NEW_WS
 	  pp cmd = %(swaymsg move container to workspace #{new_ws})
