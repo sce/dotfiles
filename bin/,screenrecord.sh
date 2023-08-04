c01fb4116bfbbba22c3bb05dd23384f896cc1a01
S. Christoffer Eliesen <christoffer.eliesen@pexip.com>
2023-08-04 10:24:24 +0200

    bin/,screenrecord: Add, with small gui.

---
+++ bin/,screenrecord.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# https://linuxconfig.org/how-to-propagate-a-signal-to-child-processes-from-a-bash-script
+#
+# We trap SIGINT and SIGTERM and then send SIGINT to everything in this process
+# group (kill 0) which will cause the recording to gracefully exit.
+#
+# Because this script is also part of the process group we have to first
+# redefine the trap handler to nothing before executing it.
+trap 'trap " " SIGINT SIGTERM; kill -SIGINT 0; wait' SIGINT SIGTERM
+
+function wait_for_user_ok {
+    name=$(zenity --info --title="Stop recording" --text="Hit OK to stop the recording")
+}
+
+~/bin/screenrecord.sway &
+
+wait_for_user_ok
+kill -s SIGINT 0
+wait
