140620c9d26de10e762a56fa65cc18a1202ccf82
S. Christoffer Eliesen <christoffer.eliesen@pexip.com>
2023-12-07 12:27:27 +0100

    bin/sway-ws-name: Show error message if any.

--- bin/sway-ws-name.sh
+++ bin/sway-ws-name.sh
@@ -12,12 +12,32 @@ function get_name {
     name=$(zenity --entry --title="Rename current workspace" --text="Enter new name of current workspace:")
 }
 
+function show_error {
+    echo "$1"
+    zenity --error --title="Failed" --text="$1"
+}
+
+function run {
+    echo "$1"
+    set +e
+    error=$($1)
+
+    if [[ $? -eq -0 ]]; then
+        error=""
+    fi
+    set -e
+}
+
 get_name
 
-if [[ "$name" != "" ]]; then
-    cmd="swaymsg rename workspace to \"$name\""
-    echo $cmd
-    $cmd
-else
+if [[ "$name" == "" ]]; then
     echo "No name given; cancelled."
+    exit 1
+fi
+
+run "swaymsg --pretty rename workspace to \"$name\""
+
+if [[ "$error" != "" ]]; then
+    show_error "$error"
+    exit 2
 fi
