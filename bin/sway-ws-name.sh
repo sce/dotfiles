56013e937f3cc6df1b0f1fdadd52b61992bcd866
S. Christoffer Eliesen <christoffer.eliesen@pexip.com>
2024-01-15 14:08:59 +0100

    bin/sway-ws-name.sh: Retry if not successful.

--- bin/sway-ws-name.sh
+++ bin/sway-ws-name.sh
@@ -9,8 +9,14 @@ set -euo pipefail
 
 
 function get_name {
-    name=$(zenity --entry --title="Rename current workspace" --text="Enter new name of current workspace:")
+    name=$(zenity --entry --title="Rename current workspace" --text="Enter new name of current workspace:" --ok-label="Rename")
 }
+
+function get_name_error {
+    local msg=$1
+    name=$(zenity --entry --title="Rename current workspace" --text="$msg\n\nEnter new name of current workspace:" --ok-label="Rename")
+}
+
 
 function show_error {
     echo "$1"
@@ -24,9 +30,18 @@ function run {
 
     if [[ $? -eq -0 ]]; then
         error=""
+    else
+        >&2 echo "$error"
     fi
     set -e
 }
+
+function run_rename {
+    local name="$1"
+    run "swaymsg --pretty rename workspace to \"$name\""
+}
+
+################################################################################
 
 get_name
 
@@ -35,9 +50,15 @@ if [[ "$name" == "" ]]; then
     exit 1
 fi
 
-run "swaymsg --pretty rename workspace to \"$name\""
+run_rename "$name"
 
-if [[ "$error" != "" ]]; then
-    show_error "$error"
-    exit 2
-fi
+# continue retrying until we are successful or the user quits:
+while [[ "$error" != "" ]]; do
+    #show_error "$error"
+    get_name_error "$error"
+    if [[ "$name" == "" ]]; then
+        echo "No name given; cancelled."
+        exit 1
+    fi
+    run_rename "$name"
+done
