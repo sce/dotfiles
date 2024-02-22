a166e0f89f31dd43ee76297eb2d30bef5e8f909b
S. Christoffer Eliesen <christoffer.eliesen@pexip.com>
2024-02-22 11:11:18 +0700

    bin/backup-simple.sh: Also special things; nicer output.

--- bin/backup-simple.sh
+++ bin/backup-simple.sh
@@ -31,7 +31,7 @@ fi
 # -H --hard-links preserve hard links
 #opts="--archive --one-file-system --hard-links --human-readable --verbose --itemize-changes --progress"
 #opts="--archive --one-file-system --hard-links --human-readable --verbose --progress"
-opts=(--archive --one-file-system --hard-links --human-readable --progress)
+opts=(--archive --one-file-system --hard-links --specials --human-readable --human-readable --partial --progress --info=stats1,progress2)
 
 # last argument should be destination dir:
 dest_dir=$BASH_ARGV
