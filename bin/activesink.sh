002427c6916a6aa41102ed021e1bd30a77381ec2
S. Christoffer Eliesen <christoffer.eliesen@pexip.com>
2024-02-25 12:54:50 +0700

    bin/activesink.sh: Use pamixer.

--- bin/activesink.sh
+++ bin/activesink.sh
@@ -2,43 +2,32 @@
 
 set -euo pipefail
 
-#ACTIVE_SINK="@DEFAULT_SINK@"
-ACTIVE_SINK=`pactl info| awk '/Default Sink:/{ print $3 }'`
-
-if [ "$ACTIVE_SINK" == "" ]; then
-  notify-send -u critical "Volume: Failed to fetch active sink"
-  exit 1
-fi
-
 function volume_text {
-  echo $(pactl list sinks|awk '/\tVolume/{ print $5 } /Mute/{ print "mute:" $2 } /Description/{ print $2 } /Formats/{ print "\r" }')
+  echo $(pamixer --get-volume-human)
 }
 
 function notify_volume {
   notify-send -u low -t 1000 "$(volume_text)"
 }
 
-INC="3%"
-function volup { pactl set-sink-volume $ACTIVE_SINK +$INC; }
-function voldown { pactl set-sink-volume $ACTIVE_SINK -$INC; }
-function mutetoggle { pactl set-sink-mute $ACTIVE_SINK toggle; unset MUTED; }
+INC="3"
 
 case "$0" in
   *up)
-    volup
+    pamixer --increase $INC
     notify_volume
     ;;
 
   *down)
-    voldown
+    pamixer --decrease $INC
     notify_volume
     ;;
 
   *mutetoggle)
-    mutetoggle
+    pamixer --toggle-mute
     notify_volume
     ;;
   *)
-    echo $ACTIVE_SINK
+    exec pamixer --get-default-sink
     ;;
 esac
