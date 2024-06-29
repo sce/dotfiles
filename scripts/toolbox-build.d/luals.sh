ec2c343e97bf087748a77b4829ef4662b279bc0d
S. Christoffer Eliesen <christoffer@eliesen.no>
2024-06-29 08:23:43 +0200

    toolbox-build.d/luals: Properly install.

--- scripts/toolbox-build.d/luals.sh
+++ scripts/toolbox-build.d/luals.sh
@@ -5,7 +5,8 @@
 set -euo pipefail
 
 tmp_dir=${1:-$(mktemp -d)}
-destination_dir=$HOME/.local/bin
+destination_dir=$HOME/.local/lua-language-server
+destination_bin=$HOME/.local/bin
 
 repo_url="https://github.com/LuaLS/lua-language-server.git"
 version="3.9.3"
@@ -30,10 +31,40 @@ function build_luals {
 
     cd lua-language-server
     ./make.sh
-
-    cp -vi bin/lua-language-server "$destination_dir" || true
   )
+}
+
+function install_luals {
+  (
+    set -x
+
+    [[ -d "$destination_dir" ]] && rm -rI "$destination_dir"
+    mkdir -p "$destination_dir"
+
+    cd "$tmp_dir"/lua-language-server
+    # These files are in the release tar ball and are needed to make the
+    # install work:
+    cp -R main.lua debugger.lua bin locale meta script "$destination_dir"
+
+    ln -sf "$destination_dir"/bin/lua-language-server "$destination_bin"
+  )
+
+  echo -e "\nInstalled lua-language-server into:"
+  echo "    $destination_dir"
+  echo "    $destination_bin"
+}
+
+function cleanup {
+  echo -e "\nThe temporary files can safely be deleted. Remove $tmp_dir ?"
+  select yn in "Yes" "No"; do
+      case $yn in
+          Yes) rm -fr "$tmp_dir"; break;;
+          No) break;;
+      esac
+  done
 }
 
 install_deps
 build_luals
+install_luals
+cleanup
