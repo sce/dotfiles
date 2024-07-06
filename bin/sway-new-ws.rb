efe527c9bfcd6cdde08c32fa88904e79b2d3b323
S. Christoffer Eliesen <christoffer@eliesen.no>
2024-07-06 22:24:18 +0200

    bin/sway-new-ws: Use numbers instead of hex for workspaces.

--- bin/sway-new-ws.rb
+++ bin/sway-new-ws.rb
@@ -28,7 +28,12 @@ def ws_name(num)
   end
 end
 
-WS=%r(sway-ws\.rb\z)
+def parse_ws_name(str)
+  return str.to_i
+  from_hex(str)
+end
+
+GOTO_WS=%r(sway-ws\.rb\z)
 NEW_WS=%r(sway-new-ws\.rb\z)
 MOVE_TO_NEW_WS=%r(sway-move-new-ws\.rb\z)
 
@@ -38,9 +43,10 @@ wss = JSON.parse(%x(swaymsg -t get_workspaces))
 # are given the number -1 in i3.
 #pp ws_nums = wss.map { |ws| ws["num"] }.reject {|num| num < 0 }.sort
 print "ws_names:"
-pp ws_names = wss.map { |ws| from_hex(ws["name"]) }.filter { |num| num != 0 }.sort
+#pp ws_names = wss.map { |ws| from_hex(ws["name"]) }.filter { |num| num != 0 }.sort
+pp ws_names = wss.map { |ws| parse_ws_name(ws["name"]) }.filter { |num| num != 0 }.sort
 
-if $0 =~ WS
+if $0 =~ GOTO_WS
 	# Create notification with the name of the current workspace.
 	if ws = wss.select { |ws| ws["focused"] }.first
 		notify "Workspace %s" % ws["name"]
@@ -49,11 +55,15 @@ if $0 =~ WS
 	end
 else
 	gap_ws = nil
-    #gap_exists = ws_nums.find.each_with_index { |num, i| num != (gap_ws = i+1) }
-    gap_exists = ws_names.find.each_with_index { |name, i| hex(name) != (gap_ws = hex(i+1)) }
-    new_ws = gap_exists && gap_ws || hex((ws_names.last || 0) + 1)
-    #new_ws = gap_exists && gap_ws || (ws_nums.last + 1)
-    #new_ws = ws_name(new_ws)
+
+	#gap_exists = ws_nums.find.each_with_index { |num, i| num != (gap_ws = i+1) }
+	#gap_exists = ws_names.find.each_with_index { |name, i| hex(name) != (gap_ws = hex(i+1)) }
+	#new_ws = gap_exists && gap_ws || hex((ws_names.last || 0) + 1)
+	gap_exists = ws_names.find.each_with_index { |name, i| ws_name(name) != (gap_ws = ws_name(i+1)) }
+	new_ws = gap_exists && gap_ws || ws_name((ws_names.last || 0) + 1)
+
+	#new_ws = gap_exists && gap_ws || (ws_nums.last + 1)
+	#new_ws = ws_name(new_ws)
 
 	if $0 =~ MOVE_TO_NEW_WS
 	  pp cmd = %(swaymsg move container to workspace #{new_ws})
