7b988dab85a10c9045a77ad60dd7af1615a24f5f
S. Christoffer Eliesen <christoffer@eliesen.no>
2024-07-19 22:03:28 +0200

    toolbox-rust.d/packages.sh: Extract from toolbox-rust.

---
+++ scripts/toolbox-rust.d/packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+packages=(
+  neovim vim
+  keychain
+  jq entr
+  # these give us ansi2txt for stripping color codes:
+  graphviz colorized-logs
+
+  rust rustfmt cargo
+)
+
+(
+  set -x
+  sudo dnf install -y "${packages[@]}"
+)
