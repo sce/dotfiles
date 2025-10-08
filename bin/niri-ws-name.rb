b3f9d90a14c1ccbc0ee45ed448efb508de8cb4f5
S. Christoffer Eliesen <christoffer.eliesen@pexip.com>
2025-10-08 13:56:23 +0200

    bin/niri-ws-name.rb: Add.

---
+++ bin/niri-ws-name.rb
@@ -0,0 +1,66 @@
+
+#!/bin/bash
+
+#
+# This script allows the user to easily rename the current niri workspace via a
+# gui prompt. It uses zenity to do so.
+#
+
+set -euo pipefail
+
+ask=~/bin/,zenity
+
+function get_name {
+    name=$($ask --entry --title="Rename current workspace" --text="Enter new name of current workspace:" --ok-label="Rename")
+}
+
+function get_name_error {
+    local msg=$1
+    name=$($ask --entry --title="Rename current workspace" --text="$msg\n\nEnter new name of current workspace:" --ok-label="Rename")
+}
+
+
+function show_error {
+    echo "$1"
+    $ask --error --title="Failed" --text="$1"
+}
+
+function run {
+    echo "$1"
+    set +e
+    error=$($1)
+
+    if [[ $? -eq -0 ]]; then
+        error=""
+    else
+        >&2 echo "$error"
+    fi
+    set -e
+}
+
+function run_rename {
+    local name="$1"
+    run "niri msg action set-workspace-name $name"
+}
+
+################################################################################
+
+get_name
+
+if [[ "$name" == "" ]]; then
+    echo "No name given; cancelled."
+    exit 1
+fi
+
+run_rename "$name"
+
+# continue retrying until we are successful or the user quits:
+while [[ "$error" != "" ]]; do
+    #show_error "$error"
+    get_name_error "$error"
+    if [[ "$name" == "" ]]; then
+        echo "No name given; cancelled."
+        exit 1
+    fi
+    run_rename "$name"
+done
