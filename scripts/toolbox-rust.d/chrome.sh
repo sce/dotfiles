ba4d24361fa80581885f8e6993749875d18e4062
S. Christoffer Eliesen <christoffer.eliesen@pexip.com>
2026-01-27 14:02:38 +0100

    scripts/toolbox-rust chrome: Update.

--- scripts/toolbox-rust.d/chrome.sh
+++ scripts/toolbox-rust.d/chrome.sh
@@ -9,7 +9,8 @@ packages=(
 # https://docs.fedoraproject.org/en-US/quick-docs/installing-chromium-or-google-chrome-browsers/
 function enable_repo {
   sudo dnf install -y fedora-workstation-repositories
-  sudo dnf config-manager --set-enabled google-chrome
+  # sudo dnf config-manager --set-enabled google-chrome
+  sudo dnf config-manager setopt google-chrome.enabled=1
 }
 
 (
