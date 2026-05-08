ba589493e444640b412bf16857a0136f5e1f3c90
S. Christoffer Eliesen <christoffer.eliesen@pexip.com>
2026-05-08 13:54:13 +0200

    .config/nvim leap: Fix backwards search.

--- .config/nvim/vim.d/leap.lua
+++ .config/nvim/vim.d/leap.lua
@@ -1,6 +1,14 @@
 -- https://codeberg.org/andyg/leap.nvim
-vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
-vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+-- vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+-- vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+
+vim.keymap.set({ 'n', 'x', 'o' }, 's', function()
+  require('leap').leap {}
+end)
+vim.keymap.set({ 'n', 'x', 'o' }, 'S', function()
+  require('leap').leap { backward = true }
+end)
+
 
 vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' }) -- or some grey
 vim.api.nvim_set_hl(0, 'LeapMatch', {
